9918e7e243b5b8083ff696aa8e29e26806396384
roy <loitp@galaxyjoy.vn>
2025-04-26 16:36:10 +0700

    update

--- app/src/main/java/com/mckimquyen/ui/ActSettings.java
+++ app/src/main/java/com/mckimquyen/ui/ActSettings.java
@@ -163,8 +163,8 @@ public class ActSettings extends ActBase implements Observer, ColorChooserDialog
             boolean hasRead = utilSettings.getBoolean(UtilSettings.KEY_READ_POLICY);
             if (!hasRead) {
                 showDialog2(this, getString(R.string.terms_and_privacy_policy), getString(R.string.read_policy), getString(R.string.agree_and_continue), getString(R.string.cancel), () -> {
-                    openUrlInBrowser(this, URL_POLICY_NOTION, getString(R.string.terms_and_privacy_policy), false);
                     utilSettings.save(UtilSettings.KEY_READ_POLICY, true);
+                    openUrlInBrowser(this, URL_POLICY_NOTION, getString(R.string.terms_and_privacy_policy), false);
                 }, () -> {
                     utilSettings.save(UtilSettings.KEY_READ_POLICY, true);
                 });
